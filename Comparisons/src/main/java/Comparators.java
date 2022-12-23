c48886e4639b673093dd0508485a39e40a13107e
Neshawhit <neshawhitmire@gmail.com>
2022-12-23 22:25:42 +0000

    Saved progress on Comparisons 2022-12-23 22:25:42 +0000

--- Comparisons/src/main/java/Comparators.java
+++ Comparisons/src/main/java/Comparators.java
@@ -14,12 +14,8 @@ public class Comparators {
      * @return true if a is equal to b, false otherwise.
      */
     public boolean isEqual(int a, int b){
-        return true;
-    }
-       public boolean isNotEqual( int a, int b){
         return false;
-
-       }
+    }
 
     /**
      * @param a the first number to be compared.
@@ -27,8 +23,7 @@ public class Comparators {
      * @return true if a is greater than b, false otherwise.
      */
     public boolean greaterThan(int a, int b){
-        // return false;
-        return true;
+        return false;
     }
     /**
      * @param a the first number to be compared.
@@ -36,8 +31,7 @@ public class Comparators {
      * @return true if a is less than b, false otherwise.
      */
     public boolean lessThan(int a, int b){
-        // return false;
-        return true;
+        return false;
     }
     /**
      * @param a the first number to be compared.
@@ -45,8 +39,7 @@ public class Comparators {
      * @return true if a is greater than or equal to b, false otherwise.
      */
     public boolean greaterThanOrEqualTo(int a, int b){
-        // return false;
-        return true;
+        return false;
     }
     /**
      * @param a the first number to be compared.
@@ -54,8 +47,7 @@ public class Comparators {
      * @return true if a is less than or equal to b, false otherwise.
      */
     public boolean lessThanOrEqualTo(int a, int b){
-        // return false;
-         return true;
+        return false;
     }
 
     /**
@@ -64,7 +56,6 @@ public class Comparators {
      * @return true if a is not equal to b, false otherwise.
      */
     public boolean notEqualTo(int a, int b){
-        // return false;
-        return true;
+        return false;
     }
 }
